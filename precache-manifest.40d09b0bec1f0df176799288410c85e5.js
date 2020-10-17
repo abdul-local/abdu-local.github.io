b37e8b81361597e973ca312e7cf5c1ad1a714960
abdul-local <abdullah.hamzan@gmail.com>
2020-10-17 14:01:39 +0800

    Deploy project

---
+++ precache-manifest.40d09b0bec1f0df176799288410c85e5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "580e5b788376d1170b39",
+    "url": "/static/css/main.84626b9f.chunk.css"
+  },
+  {
+    "revision": "580e5b788376d1170b39",
+    "url": "/static/js/main.580e5b78.chunk.js"
+  },
+  {
+    "revision": "1e88431f4fa62764bb31",
+    "url": "/static/js/1.1e88431f.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9b8237e2e393b3a0137e714f8098ca32",
+    "url": "/index.html"
+  }
+];
